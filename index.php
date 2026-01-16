b7e188e88a8466aed2b0f40d49a295af819d5ee3
iamrizkii <saefulrizky373@gmail.com>
2026-01-16 23:15:15 +0800

    perubahan

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $currentUser = getCurrentUser();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Sistem Pemantauan Kualitas Udara dalam Ruangan</title>
+  <title>Sistem Pemantauan Kualitas Udara dalam Ruangann</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
